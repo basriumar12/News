0310e379a65651ca1e782556c5ebbe77ded918a1
basri umar <basriumar12@gmail.com>
2020-04-21 05:16:39 +0700

    a

--- app/src/main/java/id/bas/news/ApiService.kt
+++ app/src/main/java/id/bas/news/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-
+//
     @GET("exec")
     fun getNews (@Query("id") id : String,
                  @Query("sheet") sheet : String
